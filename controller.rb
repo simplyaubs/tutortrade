0f8d425fb4d092cf27dc6b8ca880cef790bba598
Aubrey Howell <simplyaubs@gmail.com>
2013-10-24 23:37:48 -0600

    Add post & get for Tutor page

--- controller.rb
+++ controller.rb
@@ -2,10 +2,30 @@ require './davinci-sinatra.rb'
 
 get "/" do
   halt erb(:index)
+
 end
 
 get "/tutors/:id" do
-	id = params[:id]
+	id 		= params[:id]
 	@user = User.find(id)
 	halt erb(:show)
+end
+
+get "/tutors" do	
+	@tutors = User.all
+	halt erb(:tutors)
+end
+
+post "/tutors" do
+	@tutor 						= User.new
+	@tutor.first_name = params["first_name"]
+	@tutor.last_name  = params["last_name"]
+	@tutor.subject    = params["subject"]
+	@tutor.major			= params["major"]
+	@tutor.save!
+	redirect "/thankyou"
+end
+
+get "/thankyou" do
+	halt erb(:thankyou)
 end
